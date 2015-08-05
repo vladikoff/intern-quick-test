7227ce787c5f874f0e7538daa29550086817d266
vladikoff <vlad.filippov@gmail.com>
2015-08-05 15:41:08 -0400

    noooooooooooode

--- intern.js
+++ intern.js
@@ -2,7 +2,7 @@ define({
   proxyPort: 10232,
   proxyUrl: 'http://localhost:10232',
   suites: ['test1', 'test2', 'test3'],
-  reporters: [ 'teamcity' ],
+  reporters: [ 'Console' ],
   functionalSuites: [],
   environments: [ { browserName: 'firefox' }],
   excludeInstrumentation: true
