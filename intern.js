bd7c34786faaf7a713c1bf96d696d38518dab233
vladikoff <vlad.filippov@gmail.com>
2015-08-25 14:01:59 -0400

    add reload test

--- intern.js
+++ intern.js
@@ -1,11 +1,27 @@
-define({
-  proxyPort: 10232,
-  proxyUrl: 'http://localhost:10232',
-  suites: [],
-  functionalSuites: [ 'testfunc1'],
-  environments: [ { browserName: 'firefox' }],
-  excludeInstrumentation: true,
-  tunnelOptions: {
-    pathname: '/'
+define([
+  'intern'
+],
+function (intern) {
+  var args = intern.args;
+
+  var config = {
+    proxyPort: 10232,
+    proxyUrl: 'http://localhost:10232',
+    suites: [],
+    functionalSuites: ['testfunc1'],
+    environments: [{browserName: 'firefox'}],
+    excludeInstrumentation: true,
+    tunnelOptions: {
+      pathname: '/'
+    },
+    capabilities: {}
+  };
+
+  // custom Firefox binary location, if specified then the default is ignored.
+  // ref: https://code.google.com/p/selenium/wiki/DesiredCapabilities#WebDriver
+  if (args.firefoxBinary) {
+    config.capabilities.firefox_binary = args.firefoxBinary; //eslint-disable-line camelcase
   }
+
+  return config;
 });
