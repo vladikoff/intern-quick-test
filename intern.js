6a8a4f607993fbdfa2881abeb9ed30b5a2a23202
vladikoff <vlad.filippov@gmail.com>
2015-08-25 11:27:37 -0400

    update config

--- intern.js
+++ intern.js
@@ -2,7 +2,6 @@ define({
   proxyPort: 10232,
   proxyUrl: 'http://localhost:10232',
   suites: ['test1', 'test2', 'test3'],
-  reporters: [ 'teamcity' ],
   functionalSuites: [],
   environments: [ { browserName: 'firefox' }],
   excludeInstrumentation: true,
